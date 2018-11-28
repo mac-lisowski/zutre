3b082edb2398e68c190f3fbe92a94d2fda2d94b8
Maciej Lisowski <admin@pikion.com>
2018-11-29 04:54:52 +0800

    new component Code

---
+++ src/components/Code/Code.vue
@@ -0,0 +1,25 @@
+<template>
+  <pre class="code" :data-lang="whatLang"><code><slot></slot></code></pre>
+</template>
+
+<script>
+/**
+ * ZCode
+ * 
+ * Spectre: https://picturepan2.github.io/spectre/elements/code.html
+ */
+export default {
+  name: 'ZCode',
+  props: {
+    lang: {
+      type: String,
+      default: () => ''
+    }
+  },
+  computed: {
+    whatLang: function() {
+      return this.lang
+    }
+  }
+}
+</script>
