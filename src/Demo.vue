be8290c845297ee5d238c3619f227df8bd362b2b
Maciej Lisowski <admin@pikion.com>
2018-11-28 19:44:19 +0800

    updates in docs and Demo.vue

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-  <z-breadcrumbs :items="[{ name: 'Dashboard', href: '/'}, { name: 'Settings', href: '/settings'}]" />
-  
-  <z-breadcrumbs>
-    <z-breadcrumbs-item name="Test" href="/test" />
-    <!-- <z-breadcrumbs-item name="Test 2" :link="{ to: 's'}" /> -->
-    <z-breadcrumbs-item name="Test 2" href="/all-good" />
-    <z-breadcrumbs-item href="/custom">custom</z-breadcrumbs-item>
-  </z-breadcrumbs>
+    <h1>Zutre</h1>
+    <h2>Lightweight UI components for Vue.js based on Spectre CSS Framework</h2>
 
+    <strong>In Development</strong>
+
+    <z-breadcrumbs :items="[{ name: 'Dashboard', href: '/'}, { name: 'Settings', href: '/settings'}]" />
+    
+    <z-breadcrumbs>
+      <z-breadcrumbs-item name="Test" href="/test" />
+      <!-- <z-breadcrumbs-item name="Test 2" :link="{ to: 's'}" /> -->
+      <z-breadcrumbs-item name="Test 2" href="/all-good" />
+      <z-breadcrumbs-item href="/custom">custom</z-breadcrumbs-item>
+    </z-breadcrumbs>
+    
   </div>
 </template>
 <script>
